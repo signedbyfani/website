cd1bde7e685c85f47a3fdc64236421cf38521824
Fanindra Maharana <imfanindra@gmail.com>
2025-03-20 22:56:19 +0530

    added animation

--- components/card/index.tsx
+++ components/card/index.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 interface CardProps {
   title: string;
   body?: string;
@@ -6,7 +8,13 @@ interface CardProps {
 }
 
 export const Card = ({ title, body = "", imageSrc, cardClassName }: CardProps) => (
-  <div className={`group relative flex h-auto flex-col gap-y-4 overflow-hidden ${cardClassName}`}>
+  <motion.div
+    initial={{ scale: 0 }}
+    whileInView={{ scale: 1 }}
+    transition={{ duration: 0.5, ease: "easeOut" }}
+    viewport={{ once: true }}
+    className={`group relative flex h-auto flex-col gap-y-4 overflow-hidden ${cardClassName}`}
+  >
     <div className="relative w-full flex-1">
       <img src={imageSrc} alt={title} className="aspect-[4/3] w-full rounded-lg object-cover" />
     </div>
@@ -15,5 +23,5 @@ export const Card = ({ title, body = "", imageSrc, cardClassName }: CardProps) =
       <h3 className="w-full text-start text-default hover:decoration-none">{title}</h3>
       <p className="-mt-0 w-full text-start text-muted2 hover:decoration-none">{body}</p>
     </div>
-  </div>
+  </motion.div>
 );
