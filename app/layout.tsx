2b3f2d3bac830160ac5db014f11decde05423161
Fanindra Maharana <78368113+signedbyfani@users.noreply.github.com>
2025-05-21 11:23:12 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
       </head>
       <body>
         <RootProvider>
-          <main className="mx-auto max-w-screen-md overflow-x-hidden px-6 py-24 md:overflow-x-visible ">
+          <main className="mx-auto max-w-screen-lg overflow-x-hidden px-6 py-24 md:overflow-x-visible ">
             <article className="article">{children}</article>
           </main>
         </RootProvider>
